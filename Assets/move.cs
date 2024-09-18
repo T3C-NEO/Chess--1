1c0d14f3af0ebb81c905f46369cf7db8745f1bbd
T3CNEO <neoshon421@gmail.com>
2024-09-18 02:04:55 -0400

    work

--- Assets/move.cs
+++ Assets/move.cs
@@ -12,10 +12,19 @@ public class move : MonoBehaviour
     public GameObject[] row6;
     public GameObject[] row7;
     public GameObject[] row8;
-    public GameObject[] colE;
+
+    public GameObject[] col1;
+    public GameObject[] col2;
+    public GameObject[] col3;
+    public GameObject[] col4;
+    public GameObject[] col5;
+    public GameObject[] col6;
+    public GameObject[] col7;
+    public GameObject[] col8;
 
 
     public List<GameObject[]> rows = new List<GameObject[]>();
+    public List<GameObject[]> cols = new List<GameObject[]>();
 
     public int posX = 2;
     public int posY = 4;
@@ -31,6 +40,15 @@ public class move : MonoBehaviour
         rows.Add(row6);
         rows.Add(row7);
         rows.Add(row8);
+
+        cols.Add(col1);
+        cols.Add(col2);
+        cols.Add(col3);
+        cols.Add(col4);
+        cols.Add(col5);
+        cols.Add(col6);
+        cols.Add(col7);
+        cols.Add(col8);
     }
 
     // Update is called once per frame
@@ -42,18 +60,44 @@ public class move : MonoBehaviour
         if (Physics.Raycast(beam, out hit) && Input.GetMouseButtonDown(0) && hit.rigidbody != null)
         {
             if (hit.rigidbody.tag == "Rook")
-            {
-                for(int i = posX; i < 8; i++)
+            {      
+                for (int i = posX; i <= 7; i++)
                 {
-                    rows[posX].s
+                    rows[posY - 1][i].SetActive(true);
+                }
+                for (int i = posX - 2; i >= 0; i--)
+                {
+                    rows[posY - 1][i].SetActive(true);
+                }
+            
+                for (int i = posY; i <= 7; i++)
+                {
+                    cols[posX - 1][i].SetActive(true);
+                }
+                for (int i = posY - 2; i >= 0; i--)
+                {
+                    cols[posX - 1][i].SetActive(true);
                 }
             }
-            transform.position = new Vector3(hit.rigidbody.transform.position.x, transform.position.y, hit.rigidbody.transform.position.z);
 
-            posX = hit.rigidbody.GetComponent<local>().posX;
-            posY = hit.rigidbody.GetComponent<local>().posY;
+            if (hit.rigidbody.tag == "Finish")
+            {
+                
+                transform.position = new Vector3(hit.rigidbody.transform.position.x, transform.position.y, hit.rigidbody.transform.position.z);
+                posX = hit.rigidbody.GetComponent<local>().posX;
+                posY = hit.rigidbody.GetComponent<local>().posY;
+                for (int i = 0; i < rows[posY - 1].Length; i++)
+                {
+                    rows[posY - 1][i].SetActive(false);
+                }
+                for (int i = 0; i < cols[posX - 1].Length; i++)
+                {
+                    cols[posX - 1][i].SetActive(false);
+                }
+            }
+            
 
-            Debug.Log("d");
+            /*
             for (int i = 0; i < row4.Length; i++)
             {
                 if (row4[i] == hit.rigidbody.gameObject)
@@ -63,6 +107,7 @@ public class move : MonoBehaviour
                     break;
                 }
             }
+            */
         }
     }
 }
